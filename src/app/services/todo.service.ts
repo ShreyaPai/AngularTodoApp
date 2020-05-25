0a9f61d3e2d677837a2cf2a81565417c5bcab877
Shreya Pai <ayerhs1997@gmail.com>
2020-05-25 20:08:45 +0530

    add a configuration file

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,24 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TodoModel } from '../models/todo.model';
 import { Observable } from 'rxjs';
+import { Configuration } from '../configuration'
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
-};
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type': 'application/json',
+//   }),
+// };
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  url = 'https://jsonplaceholder.typicode.com/todos?_limit=5';
-  // limit = '';
+  url = Configuration.url;
+
+   // limit = '';
 
   constructor(private http: HttpClient) {}
 
   addTodo(todo: TodoModel) {
-    return this.http.post<TodoModel>(this.url, todo, httpOptions);
+    console.log(Configuration.url);
+    return this.http.post<TodoModel>(this.url, todo);
   }
 
   getTodoList(): Observable<TodoModel[]> {
@@ -28,11 +31,11 @@ export class TodoService {
 
   isComplete(todo: TodoModel): Observable<any> {
     const url = `${this.url}/${todo.id}`;
-    return this.http.put(url, todo, httpOptions);
+    return this.http.put(url, todo);
   }
 
   deleteTodo(todo: TodoModel): Observable<TodoModel> {
     const url = `${this.url}/${todo.id}`;
-    return this.http.delete<TodoModel>(url, httpOptions);
+    return this.http.delete<TodoModel>(url);
   }
 }
